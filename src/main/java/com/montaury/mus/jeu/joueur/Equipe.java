f093e5f489d2549aec28c0acf1f910623b177b7d
Hugo Rivas <hrgeslin@iutbayonne.univ-pau.fr>
2022-02-09 17:43:35 +0100

    Equipe created

--- src/main/java/com/montaury/mus/jeu/joueur/Equipe.java
+++ src/main/java/com/montaury/mus/jeu/joueur/Equipe.java
@@ -3,6 +3,19 @@ package com.montaury.mus.jeu.joueur;
 import java.util.List;
 
 public class Equipe {
-    private List<Joueur> joueurs;
+    private Joueur joueurA;
+    private Joueur joueurB;
 
+    public Equipe (Joueur joueurA, Joueur joueurB){
+        this.joueurA = joueurA;
+        this.joueurB = joueurB;
+    }
+
+    public Joueur getJoueurA() {
+        return joueurA;
+    }
+
+    public Joueur getJoueurB() {
+        return joueurB;
+    }
 }
