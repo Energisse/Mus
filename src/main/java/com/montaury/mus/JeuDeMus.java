42d260083a0d880ba80f637366b3471d0c4638f9
Hugo Rivas <hrgeslin@iutbayonne.univ-pau.fr>
2022-02-09 17:45:08 +0100

    Equipe implemented in JeuDeMus.java, Joueur.java & Opposants.java

--- src/main/java/com/montaury/mus/JeuDeMus.java
+++ src/main/java/com/montaury/mus/JeuDeMus.java
@@ -2,6 +2,7 @@ package com.montaury.mus;
 
 import com.montaury.mus.jeu.Partie;
 import com.montaury.mus.console.AffichageEvenements;
+import com.montaury.mus.jeu.joueur.Equipe;
 import com.montaury.mus.jeu.joueur.Joueur;
 import com.montaury.mus.jeu.Opposants;
 import java.util.Scanner;
@@ -12,8 +13,11 @@ public class JeuDeMus {
     var nomJoueur = new Scanner(System.in).next();
     var joueurHumain = Joueur.humain(nomJoueur);
 
+    var equipeHumain = new Equipe(joueurHumain, Joueur.ordinateur("Co-equipier Bruno"));
+    var equipeOrdinateurs = new Equipe(Joueur.ordinateur("Opposant Pedro"), Joueur.ordinateur("Opposant Alex"));
+
     var partie = new Partie(new AffichageEvenements(joueurHumain));
-    var resultat = partie.jouer(new Opposants(joueurHumain, Joueur.ordinateur()));
+    var resultat = partie.jouer(new Opposants(equipeHumain, equipeOrdinateurs));
 
     System.out.println("Le vainqueur de la partie est " + resultat.vainqueur().nom());
   }
--- src/main/java/com/montaury/mus/jeu/Opposants.java
+++ src/main/java/com/montaury/mus/jeu/Opposants.java
@@ -1,33 +1,53 @@
 package com.montaury.mus.jeu;
 
+import com.montaury.mus.jeu.joueur.Equipe;
 import com.montaury.mus.jeu.joueur.Joueur;
+
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Opposants {
-  private Joueur joueurEsku;
-  private Joueur joueurZaku;
+  private final LinkedList<Joueur> joueursOrdre = new LinkedList<>();
 
-  public Opposants(Joueur joueurEsku, Joueur joueurZaku) {
-    this.joueurEsku = joueurEsku;
-    this.joueurZaku = joueurZaku;
+  public Opposants(Equipe equipeEsku, Equipe equipeZaku) {
+    joueursOrdre.add(0,equipeEsku.getJoueurA());  //  1 A ESKU
+    joueursOrdre.add(1,equipeZaku.getJoueurA());  //  2 A
+    joueursOrdre.add(2,equipeEsku.getJoueurB());  //  1 B
+    joueursOrdre.add(3,equipeZaku.getJoueurB());  //  2 B ZAKU
   }
 
   public void tourner() {
-    var tmp = joueurEsku;
-    joueurEsku = joueurZaku;
-    joueurZaku = tmp;
+    Joueur zakuToEsku = joueursOrdre.pollLast();    //  ZAKU
+    joueursOrdre.addFirst(zakuToEsku);
+
+
   }
 
   public Joueur joueurEsku() {
-    return joueurEsku;
+    return joueursOrdre.getFirst();
+  }
+
+  public Joueur joueurEskuCoEquipier() {
+    return joueursOrdre.get(2);
   }
 
   public Joueur joueurZaku() {
-    return joueurZaku;
+    return joueursOrdre.getLast();
   }
 
-  public List<Joueur> dansLOrdre() {
-    return List.of(joueurEsku, joueurZaku);
+  public Joueur joueurZakuCoEquipier() {
+    return joueursOrdre.get(1);
   }
+
+  public Equipe equipeEsku() {
+    return new Equipe(joueurEsku(),joueurEskuCoEquipier());
+  }
+  public Equipe equipeZaku(){
+    return new Equipe(joueurZaku(),joueurZakuCoEquipier());
+  }
+  public List<Joueur> dansLOrdre() {
+
+    return new ArrayList<>(joueursOrdre); }
 }
